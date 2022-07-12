3dc55cd4bf2bce833f4e08e4961e13e1eb294a00
Latecoder-hub <cagatay.ersoy1@gmail.com>
2022-07-12 14:55:15 -0700

    update before time

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const defaultUsers = [{
     email: "test1@test"
 },
 {
-    userName:"cadmin2",
-    phone:1233454555,
-    email:"test2@test"
-},{
-    userName:"badmin3",
-    phone:9987454555,
-    email:"test3@test"
+    userName: "cadmin2",
+    phone: 1233454555,
+    email: "test2@test"
+}, {
+    userName: "badmin3",
+    phone: 9987454555,
+    email: "test3@test"
 }
 
 ]
@@ -28,8 +28,10 @@ const View = (() => {
         email: "email",
         submit: "submit",
         search: "#search",
-        noResult:".noResult",
-        nameColumn:"#nameColumn"
+        noResult: ".noResult",
+        nameColumn: "#nameColumn",
+        error: ".error",
+
 
     }
     // render function
@@ -79,8 +81,8 @@ const Model = ((Users, view) => {
         constructor(userName, phone, email) {
             this.userName = userName
             this.phone = phone,
-            this.email = email
-                
+                this.email = email
+
         }
     }
     // create state for state management
@@ -121,61 +123,79 @@ const Controller = ((model, view) => {
     const state = new model.State()
     const searchMobile = () => {
         const searchBox = document.querySelector(view.domstr.search);
-        
+
         // return elements equal to phone number's parts
         const searchNumber = (number, arr) =>
-            arr.filter(element =>{
-                const temp=element.phone.toString()
+            arr.filter(element => {
+                const temp = element.phone.toString()
                 return number === temp.slice(0, -(temp.length - number.length))
             }
-                )
-                
+            )
+
         searchBox.addEventListener('keyup', (event) => {
-            const searchResult=event.target.value?searchNumber(searchBox.value,state.allUsers):[...state.allUsers]
-            
+            const searchResult = event.target.value ? searchNumber(searchBox.value, state.allUsers) : [...state.allUsers]
+
             const domElement = document.querySelector(view.domstr.container)
             const container1 = view.createTemplate(searchResult, view.createObjTmp)
             view.render(domElement, container1)
-           document.querySelector(view.domstr.noResult).style.display=container1?"none":"block"
-           
-           
+            document.querySelector(view.domstr.noResult).style.display = container1 ? "none" : "block"
+
+
         });
 
     }
-    const alphabeticOrder=()=>{
-        const nameButton=document.querySelector(view.domstr.nameColumn)
-        let clickCount=0
+    const alphabeticOrder = () => {
+        const nameButton = document.querySelector(view.domstr.nameColumn)
+        let clickCount = 0
         nameButton.addEventListener('click', () => {
 
-            const sorted=state.allUsers.sort(function(a, b) {
+            const sorted = state.allUsers.sort(function (a, b) {
                 return a.userName.localeCompare(b.userName);
-             });
-           
-             state.users=clickCount%2===0?[...sorted]:[...sorted].reverse()
-             clickCount++
+            });
+
+            state.users = clickCount % 2 === 0 ? [...sorted] : [...sorted].reverse()
+            clickCount++
         })
     }
 
     const addUser = () => {
-        ///^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+
         const submit = document.getElementById(view.domstr.submit)
         const name = document.getElementById(view.domstr.name)
         const email = document.getElementById(view.domstr.email)
         const phone = document.getElementById(view.domstr.mobile)
+        const checkInputValues = (name, phone, email) => {
+            const regexName = /^[a-zA-Z ]*$/
+            const regexPhone = /^[0-9]*$/
+            const regexEmail = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/
+
+            const checkName = (name) => (regexName.test(name)) && (name.length < 21)
+            const checkPhone = (phone) => regexPhone.test(phone) && phone.length < 11
+            const checkEmail = (email) => regexEmail.test(email) && email.length < 41
+            return checkName(name) && checkPhone(phone) && checkEmail(email)
+        }
         submit.addEventListener("click", () => {
             // check values of input
+            if(checkInputValues(name.value, phone.value, email.value)){
+                const newUser = new model.User(name.value, phone.value, email.value)
+                state.users = [newUser, ...state.allUsers]
+                 // reset inputs
             
-            const newUser = new model.User(name.value,  phone.value,email.value)
+                name.value = ""
+                email.value = ""
+                phone.value = ""
+            }
+            else {
+                document.querySelector(view.domstr.error).style.display = "block"
+            }
+            
 
-            state.users = [newUser,...state.allUsers]
-            // reset part
-            name.value=""
-            email.value=""
-            phone.value=""
+            
+          
         })
-        
 
-        
+
+
 
 
 
@@ -188,7 +208,7 @@ const Controller = ((model, view) => {
         const defaultUsers = model.defaultUsers
 
         state.users = [...defaultUsers]
-     
+
     }
 
     const bootstrap = () => {
